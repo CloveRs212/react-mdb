2cad1917482a2a4e1fdf3a73e11995dac55faac7
Kyle DeRusha <cloverindustrys@gmail.com>
2022-10-30 10:06:06 -0400

    Hero Hook and Implamation

---
+++ src/components/HeroImage/HeroImage.styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+
+`;
+
+export const Content = styled.div`
+
+`;
+
+export const Text = styled.div`
+
+`;
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import API from '../API';
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 // Components 
+import HeroImage from './HeroImage/image';
 
 // Hook
 import { useHomeFetch } from '../hooks/useMovieFetch'
@@ -18,7 +19,17 @@ const Home = () => {
 
   console.log(state);
 
-  return <div>Home Page</div>
+  return ( 
+    <>
+        {state.results[0] ? (
+            <HeroImage 
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                title={state.results[0].original_title}
+                text={state.results[0].overview}
+                />
+        ) : null}
+    </>
+  );
 };
 
 export default Home;
